198c66a9c7e311666f87c01131a4562ceb0258f0
PhucNHB <phucnhb519.dev@gmail.com>
2024-12-09 15:03:01 +0700

    feat: init Mango.Web resource and add BaseService

---
+++ Microservice/Mango.Web/Util/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Util
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+        }
+    }
+}
