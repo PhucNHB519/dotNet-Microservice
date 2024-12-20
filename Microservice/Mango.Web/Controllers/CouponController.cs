cca4043d9a1e7bc43569aa8582ef9923cea7d993
PhucNHB <phucnhb519.dev@gmail.com>
2024-12-20 16:24:03 +0700

    add toast notifications

--- Microservice/Mango.Web/Controllers/CouponController.cs
+++ Microservice/Mango.Web/Controllers/CouponController.cs
@@ -26,6 +26,11 @@ namespace Mango.Web.Controllers
             {
                 list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+
             return View(list);
         }
 
@@ -43,7 +48,12 @@ namespace Mango.Web.Controllers
 
                 if (response != null && response.IsSuccess)
                 {
-                    return RedirectToAction(nameof(CouponIndex));  
+                    TempData["success"] = "Coupon created successfully";
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+                else
+                {
+                    TempData["error"] = response.Message;
                 }
             }
             return View(model);
@@ -58,6 +68,12 @@ namespace Mango.Web.Controllers
                 CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
                 return View(model);
             }
+
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+
             return NotFound();
         }
 
@@ -68,8 +84,14 @@ namespace Mango.Web.Controllers
 
             if (response != null && response.IsSuccess)
             {
+                TempData["success"] = "Coupon deleted successfully";
                 return RedirectToAction(nameof(CouponIndex));
             }
+            else
+            {
+                TempData["errorR"] = response.Message;
+            }
+
             return View(couponDto);
         }
     }
